8d25c8f72e18de1bd8d8e77b6463d656488a44d8
max-de-rooij <54850292+max-de-rooij@users.noreply.github.com>
2023-10-26 18:16:06 +0200

    add campus and matlab schemes

---
+++ data/campus.jl
@@ -0,0 +1,30 @@
+# various color schemes taken from the images of the TU/e campus
+
+addcolorscheme(:atlasbynight, [
+ RGB(0.733,0.784,0.949),
+ RGB(0.518,0.627,0.851),
+ RGB(0.435,0.573,0.749),
+ RGB(0.149,0.141,0.004),
+ RGB(0.749,0.576,0.059)
+],
+ "Picture of the Atlas TU/e Building by night."
+ )
+
+addcolorscheme(:daffodils,[
+ RGB(0.949,0.886,0.02),
+ RGB(0.949,0.796,0.02),
+ RGB(0.949,0.529,0.02),
+ RGB(0.949,0.361,0.02),
+ RGB(0.051,0.0,0.0)
+],
+"Daffodils at the TU/e campus in spring."
+ )
+
+addcolorscheme(:flagsinthesun, [
+  RGB(0.545,0.004,0.165),
+  RGB(0.839,0.145,0.329),
+  RGB(0.016,0.463,0.851),
+  RGB(0.416,0.698,0.737),
+  RGB(0.733,0.839,0.906)
+],
+"Flags in the sun in front of the Atlas building.")
